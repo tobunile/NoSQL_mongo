fc57e60b320ad051fe749561064d395ff6aca671
tobunile <tobkolleg@yahoo.de>
2015-12-17 03:36:28 +0100

    Aufgabe 4 (Replication) überarbeitet und getestet

--- Aufgabe_4_Replication.sh
+++ Aufgabe_4_Replication.sh
@@ -1,5 +1,7 @@
-#todo checken ob alles in virtueller Maschine funktioniert(getestet in normalem Rechner)
+###Achtung dieses Skript funktioniert nur manchmal
+###verifizieren und evtl. nachbessern ist erforderlich
 #Ausfuehren mit Befehl nach Doppelpunkt: sudo ./Aufgabe_4_Part1.sh
+
 sudo mongod --dbpath /var/lib/mongodb/ --shutdown
 sudo killall mongod
 sudo killall mongos
@@ -16,7 +18,25 @@ mongod --replSet rs1 --logpath /home/nosql/mongo/logs/rs1-n0.log --dbpath /home/
 mongod --replSet rs1 --logpath /home/nosql/mongo/logs/rs1-n1.log --dbpath /home/nosql/mongo/rs1/n1 --port 47018 --fork --shardsvr --smallfiles
 mongod --replSet rs1 --logpath /home/nosql/mongo/logs/rs1-n2.log --dbpath /home/nosql/mongo/rs1/n2 --port 47019 --fork --shardsvr --smallfiles
 
-############An dieser Stelle kaeme Aufgabe_4_Part2.sh#################
+############Begin eigenes Zeug#################
+
+#initialisiere 1. Replicaset
+#dieser mongod wird damit dem Replicaset hinzugefuegt
+mongo --port 37017  --eval "rs.initiate()"
+
+#hinzufuegen der anderen 2 mongod im Replicaset
+mongo --port 37017  --eval "rs.add(\"nosql-VirtualBox:37018\")"
+mongo --port 37017  --eval "rs.add(\"nosql-VirtualBox:37019\")"
+
+#initialisiere 2. Replicaset
+#dieser mongod wird damit dem Replicaset hinzugefuegt
+mongo --port 47017  --eval "rs.initiate()"
+
+#hinzufuegen der anderen 2 mongod im Replicaset
+mongo --port 47017  --eval "rs.add(\"nosql-VirtualBox:47018\")"
+mongo --port 47017  --eval "rs.add(\"nosql-VirtualBox:47019\")"
+
+#################Ende eigenes Zeug#################################
 
 #Starten der Config Server
 mkdir -p /home/nosql/mongo/config/c0 /home/nosql/mongo/config/c1 /home/nosql/mongo/config/c2
@@ -25,4 +45,19 @@ mongod --logpath /home/nosql/mongo/logs/c1.log --dbpath /home/nosql/mongo/config
 mongod --logpath /home/nosql/mongo/logs/c2.log --dbpath /home/nosql/mongo/config/c2 --port 57019 --fork --configsvr --smallfiles
 
 #Starten von mongos
-mongos --logpath /home/nosql/mongo/logs/mongos.log --configdb tob-P55-UD3:57017,tob-P55-UD3:57018,tob-P55-UD3:57019 --port 27017 --fork
+sudo mongos --logpath /home/nosql/mongo/logs/mongos.log --configdb nosql-VirtualBox:57017,nosql-VirtualBox:57018,nosql-VirtualBox:57019 --port 27017 --fork
+
+##############Begin Verifizierungsphase#####################
+
+#nachsehen, ob alle Prozesse laufen
+#netstat -tnlp | sort
+
+#verifiziere 1. Replicaset
+#mongo --port 37017
+#rs.status()
+
+#verifiziere 2. Replicaset
+#mongo --port 47017
+#rs.status()
+
+
